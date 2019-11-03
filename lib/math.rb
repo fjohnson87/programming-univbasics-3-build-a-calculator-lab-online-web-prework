86a45794161faff9ad0b2ca14f7250e87e78d301
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2019-11-03 19:10:46 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,40 @@
 def addition(num1, num2)
-
+ sum = num1 + num2
+ num1 = 5
+ num2 = 4
+sum
 end
 
-def subtraction(num1, num2)
-
+def subtraction (num1 , num2)
+ difference = (num2 - num1)
+ num2 = 10
+ num1 = 5
+ print difference
 end
 
-def division(num1, num2)
-
+def division (num1 , num2)
+ quotient = (num2 / num1)
+ num2 = 50
+ num1 = 2
+ quotient
 end
 
 def multiplication(num1, num2)
-
+ product = num1 * num2
+ num1 = 4
+ num2 = 30
+product  
 end
 
 def modulo(num1, num2)
-
+  remainder =num1.modulo(num2)
+  num1 = 34
+  num2 = 5
+ remainder  
 end
 
 def square_root(num)
-
+  root = math.sqrt(num)
+  num = 81
+ answer
 end
